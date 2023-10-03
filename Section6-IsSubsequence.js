efbddf4bac1ebd35f52b26422f8928fd67165b21
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-10-03 10:26:09 +0300

    ...

--- Section6-IsSubsequence.js
+++ Section6-IsSubsequence.js
@@ -31,7 +31,15 @@ function isSubsequence(str1, str2) {
     return false;
 }
 
+function isSubsequence_Recursive(str1, str2) {
+    if (str1.length === 0) return true
+    if (str2.length === 0) return false
+    if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+    return isSubsequence(str1, str2.slice(1))
+}
+
 console.log(isSubsequence('hello', 'hello world')); // true
 console.log(isSubsequence('sing', 'sting')); // true
 console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('', 'abracadabra')); // false
 console.log(isSubsequence('abc', 'acb')); // false (order matters)
