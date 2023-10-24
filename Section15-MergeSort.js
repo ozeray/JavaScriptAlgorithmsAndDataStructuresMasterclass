8f1357b2a68ffe8ef5b2f9bf234d326a4d7e9547
Ahmet Yaşar Özer <ahmetozer10@gmail.com>
2023-10-24 14:34:05 +0300

    ...

--- Section15-MergeSort.js
+++ Section15-MergeSort.js
@@ -1,14 +1,13 @@
-function mergeArrays(arr1, arr2) {
+function mergeArraysSorted(arr1, arr2) {
     let arr = [];
     let i = 0;
     let j = 0;
 
     while(i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
+        if (arr1[i] < arr2[j])
             arr.push(arr1[i++]);
-        } else {
+        else
             arr.push(arr2[j++]);
-        }
     }
     if (i < arr1.length) arr.push(...arr1.slice(i))
     if (j < arr2.length) arr.push(...arr2.slice(j))
@@ -16,11 +15,21 @@ function mergeArrays(arr1, arr2) {
 }
 
 
-console.log(mergeArrays([-1,1], [0,6]));
-console.log(mergeArrays([-1,1], [2,3]));
-console.log(mergeArrays([1,5], [4]));
-console.log(mergeArrays([], [1,1]));
-console.log(mergeArrays([1,1], []));
-console.log(mergeArrays([], [1,2]));
-console.log(mergeArrays([-2], [1,2]));
-console.log(mergeArrays([-2,1], [2]));
+// console.log(mergeArraysSorted([-1,1], [0,6]));
+// console.log(mergeArraysSorted([-1,1], [2,3]));
+// console.log(mergeArraysSorted([1,5], [4]));
+// console.log(mergeArraysSorted([], [1,1]));
+// console.log(mergeArraysSorted([1,1], []));
+// console.log(mergeArraysSorted([], [1,2]));
+// console.log(mergeArraysSorted([-2], [1,2]));
+// console.log(mergeArraysSorted([-2,1], [2]));
+
+function splitArray(arr) {
+    let arr1 = arr.slice(0, arr.length / 2);
+    let arr2 = arr.slice(arr.length / 2, arr.length);
+    return [arr1, arr2];
+}
+
+// console.log(splitArray([-1, 1]));
+// console.log(splitArray([-1, 1, 2]));
+// console.log(splitArray([-1, 1, 2, 4]));
